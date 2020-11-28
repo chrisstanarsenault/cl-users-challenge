d921777aa28d07c4cb9eda87d36239cc8fe1ce12
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-11-27 20:24:50 -0500

    added fetch to get user data from API in created lifecycle

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,21 @@
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      users: [],
+    };
+  },
+
+  methods: {},
+  async created() {
+    await fetch("https://5fbc04c1c09c200016d4160c.mockapi.io/api/v1/customers")
+      .then((res) => res.json())
+      .then((users) => {
+        this.users = users;
+      })
+      .catch((err) => console.log(err));
+  },
 };
 </script>
 
